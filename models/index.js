9b614ac0baba2bc0c1304a3cf9c8933f1c6f2677
Iz P <pehisaac@gmail.com>
2018-05-14 12:11:30 -0600

    testing database

--- models/index.js
+++ models/index.js
@@ -8,6 +8,9 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
+console.log('this is your env variable' + env);
+
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env
   [config.use_env_variable]);
